978466183719fa7eb89af601d3fd31105e9b5a4b
acookies <39057353+acookies@users.noreply.github.com>
2019-07-27 22:32:21 +0900

    blog commit

---
+++ blog_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import Blog
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404, redirect
+from .form import BlogPost
+
+# Create your views here.
+
+def home(request):
+    blogs = Blog.objects
+    return render(request, "home.html", {'blogs' : blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html', {'blog':blog_detail})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_data = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/' + str(blog.id))
+
+def blogpost(request):
+    if request.method =='POST':
+        form = BlogPost(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.pub_date=timezone.now()
+            post.save()
+            return redirect('home')
+    else:
+        form = BlogPost()
+        return render(request,'new.html',{'form':form})
+
